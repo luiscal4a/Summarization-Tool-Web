1e393e3eeb45995ba3d42c214cb21763cb0545e2
luiscal4a <luiscal4a@gmail.com>
2022-05-16 02:46:23 +0200

    Added minimum and maximum length

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,18 @@
     <NavBar />
     <div id="content">
       <h1>Summarize any scientific paper</h1>
-      <TextInput @changeState="state = $event" @showSummary="data = $event"/>
+      <TextInput @changeState="state = $event" @showSummary="data = $event" />
       <div id="result">
-      <b-icon
-        v-if="state === 'loading'"
-        icon="arrow-clockwise"
-        animation="spin"
-        font-scale="4"
-      ></b-icon>
-      <SummarizationAccordion v-if="state === 'loaded'" :summaryItem="data"/>
-      <b-alert v-if="state === 'error'" show variant="danger" dismissible>There was an error</b-alert>
+        <b-icon
+          v-if="state === 'loading'"
+          icon="arrow-clockwise"
+          animation="spin"
+          font-scale="4"
+        ></b-icon>
+        <SummarizationAccordion v-if="state === 'loaded'" :summaryItem="data" />
+        <b-alert v-if="state === 'error'" show variant="danger" dismissible
+          >There was an error</b-alert
+        >
       </div>
     </div>
   </div>
@@ -35,7 +37,13 @@ export default {
   data() {
     return {
       state: "prev",
-      data: {"final_summary": "Hola", "summaries":["Hola","muy","buenas"]}
+      data: { 
+        final_summary: "A. Gidiotis is with the School of Informatics, Aristotle University of Thessaloniki, Greece . He and G. Tsoumakas propose a novel divideand-conquer approach that breaks both the document and its target summary into multiple smaller source-target pairs . They train a neural model that learns to summarize these smaller document parts, and then inferenceaggregates the partial summaries in order to produce a final complete summary .", 
+        summaries: [
+          "A. Gidiotis is with the School of Informatics, Aristotle University of Thessaloniki, Greece . He and G. Tsoumakas propose a novel divideand-conquer approach that breaks both the document and its target summary into multiple smaller source-target pairs . They then train a neural model that learns to summarize these smaller document parts, and then inferenceaggregates the partial summaries in order to produce a final complete summary .",
+          "Using a 3 years-old sequence-to-sequence model, our approach manages to achieve surprisingly good results, surpassing recent more advanced models [14], [15] In addition, when paired with a very strong Transformer model such as PEGASUS, our method produces results that are on par with the state-ofthe-art on both datasets . This paper is based on past work that assumed the existence of structured summaries, such as those available for some of the biomedical articles indexed in PubMed ",
+          "In cases where the input and target sequences are longer, the complexity of models that process the complete article at once increases dramatically making such methods infeasible . Different approaches attempt to solve this problem by exploiting the structure of a document . We treat each section of the text as a separate “summarization instance” and as a result our method is easily parallelizable . We use a Pre-training with Extracted Gap-sentences for Abstractive SUmmarization Sequence-to-sequence (PEGASUS) model .",
+          "A. Gidiotis is with the School of Informatics, Aristotle University of Thessaloniki, Greece . He and G. Tsoumakas propose a novel divideand-conquer approach that breaks both the document and its target summary into multiple smaller source-target pairs . They train a neural model that learns to summarize these smaller document parts, and then inferenceaggregates the partial summaries in order to produce a final complete summary ."] },
     };
   },
 };
--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -7,6 +7,39 @@
         v-model="text"
         rows="12"
       ></b-form-textarea>
+      <b-form-select
+        v-model="selected"
+        :options="options"
+        class="mb-3"
+        value-field="item"
+        text-field="name"
+      ></b-form-select>
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-none">Minimum token length:</label>
+        </b-col>
+        <b-col sm="9">
+          <b-form-input
+            v-model="min_len"
+            placeholder="Enter your name"
+            type="number"
+            class="mb-3"
+          ></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-none">Maximum token length:</label>
+        </b-col>
+        <b-col sm="9">
+          <b-form-input
+            v-model="max_len"
+            placeholder="Enter your name"
+            type="number"
+            class="mb-3"
+          ></b-form-input>
+        </b-col>
+      </b-row>
       <b-button type="submit" variant="primary" id="submit-button"
         >Submit</b-button
       >
@@ -20,25 +53,37 @@ export default {
   data() {
     return {
       text: "",
+      selected: "prophetnet",
+      min_len: 75,
+      max_len: 300,
+      options: [
+        { item: "prophetnet", name: "Prophetnet whole document summarization" },
+        { item: "distilbert", name: "Distilbert division summarization" },
+      ],
     };
   },
   methods: {
     onSubmit(event) {
-      this.holaMundo()
+      this.holaMundo();
     },
     holaMundo() {
-      let localThis = this
+      let localThis = this;
       this.$emit("changeState", "loading");
-      axios.post("http://127.0.0.1:8000/summarize/", {
-        content: localThis.text
-      }).then((result) => {
-        console.log(result.data);
-        localThis.$emit("changeState", "loaded");
-        localThis.$emit("showSummary", result.data);
-      })
-      .catch(function (error) {
-        localThis.$emit("changeState", "error");
-      });
+      axios
+        .post("http://127.0.0.1:8000/summarize/", {
+          content: localThis.text,
+          model: localThis.selected,
+          min_length: localThis.min_len,
+          max_length: localThis.max_len
+        })
+        .then((result) => {
+          console.log(result.data);
+          localThis.$emit("changeState", "loaded");
+          localThis.$emit("showSummary", result.data);
+        })
+        .catch(function (error) {
+          localThis.$emit("changeState", "error");
+        });
     },
   },
 };
